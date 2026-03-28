9c5e7ff5d9321cfdca81519e11e9ef5005b5658f
Mayank Hooda <65529547+Ma107399@users.noreply.github.com>
2026-03-28 18:38:48 +0530

    Add Role class with properties and permissions collection

---
+++ Entities/Role.cs
@@ -0,0 +1,9 @@
+namespace Vaultix.Core.Entities;
+
+public class Role
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Permission> Permissions { get; set; }
+}
